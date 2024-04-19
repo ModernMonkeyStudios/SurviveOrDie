0059be35fc195aee3e4d100fb568ff5d22159507
Michael Connelly <michaelxconnelly@gmail.com>
2024-04-19 00:40:58 -0400

    Packaging COMPLETE

---
+++ Plugins/Marketplace/Oceanology_Plugin/Source/Oceanology_Plugin/Private/Components/OceanologyWaterBodiesComponent.cpp
@@ -0,0 +1,77 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/OceanologyWaterBodiesComponent.h"
+#include "Engine/World.h"
+#include "OceanologyRuntimeSettings.h"
+#include "Actors/OceanologyManagerActor.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "Kismet/KismetMaterialLibrary.h"
+#include "Kismet/KismetRenderingLibrary.h"
+#include "Utils/OceanologyMessageUtils.h"
+#include "Utils/OceanologyRVTBaker.h"
+#include "VT/VirtualTextureBuilder.h"
+#include "VT/VirtualTexture.h"
+
+UOceanologyWaterBodiesComponent::UOceanologyWaterBodiesComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UOceanologyWaterBodiesComponent::Init()
+{
+	const UOceanologyRuntimeSettings* OceanologyRuntimeSettings = GetDefault<UOceanologyRuntimeSettings>();
+	if (!WaterBodiesRenderTarget)
+	{
+		WaterBodiesRenderTarget = OceanologyRuntimeSettings->GetRT_WaterBodies();
+	}
+	
+	AOceanologyManager* OwnerActor = Cast<AOceanologyManager>(GetOwner());
+	OwnerActor->WaterBodiesVirtualTextureComponent->SetBuildLevels(BuildLevels);
+#if WITH_EDITORONLY_DATA
+	OwnerActor->UpdateTextureSize(OwnerActor->WaterBodiesVirtualTextureComponent, TextureSize);
+#endif
+}
+
+void UOceanologyWaterBodiesComponent::CaptureRT()
+{
+	AOceanologyManager* OwnerActor = Cast<AOceanologyManager>(GetOwner());
+	if (!WaterBodiesRenderTarget)
+	{
+		return;
+	}
+
+	WaterBodiesRenderTarget->AddressY = TA_Clamp;
+	WaterBodiesRenderTarget->AddressX = TA_Clamp;
+	WaterBodiesRenderTarget->RenderTargetFormat = RTF_RGBA16f;
+
+	UTexture2D* TextureSource = OwnerActor->HeightmapVirtualTextureComponent->GetStreamingTexture()->Texture;
+	if (bUseCustomRVTBaker)
+	{
+		FOceanologyRVTBaker::BakeRVTIntoTexture(BakedWaterBodies, OwnerActor->WaterBodiesVirtualTextureComponent);
+		if (!BakedWaterBodies)
+		{
+			UOceanologyMessageUtils::CreateMessage(this, "Failed to bake RVT!", false);
+			return;
+		}
+
+		TextureSource = BakedWaterBodies;
+	}
+
+	UKismetRenderingLibrary::ResizeRenderTarget2D(
+		WaterBodiesRenderTarget,
+		TextureSource->GetSizeX(),
+		TextureSource->GetSizeY()
+	);
+
+	if (!CopyMaterialInstance)
+	{
+		CopyMaterialInstance = UKismetMaterialLibrary::CreateDynamicMaterialInstance(
+			this,
+			GetDefault<UOceanologyRuntimeSettings>()->GetRVT_WaterBodies_CopyMaterial()
+		);
+	}
+
+	CopyMaterialInstance->SetTextureParameterValue("Texture", TextureSource);
+	UKismetRenderingLibrary::DrawMaterialToRenderTarget(this, WaterBodiesRenderTarget, CopyMaterialInstance);
+}
---
+++ Plugins/Marketplace/Oceanology_Plugin/Source/Oceanology_Plugin/Public/Components/OceanologyWaterBodiesComponent.h
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "OceanologyWaterBodiesComponent.generated.h"
+
+class UWorld;
+class UMaterialParameterCollectionInstance;
+class UTextureRenderTarget2D;
+class UBoxComponent;
+class UVirtualTexture2D;
+class UVirtualTextureBuilder;
+class UTexture2D;
+
+UCLASS()
+class OCEANOLOGY_PLUGIN_API UOceanologyWaterBodiesComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UOceanologyWaterBodiesComponent();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = WaterBodies,
+		meta = (AllowPrivateAccess = "true", AdvancedDisplay))
+	bool bUseCustomRVTBaker = true;
+
+	/** Number of streaming low mips to build for the virtual texture. */
+	UPROPERTY(EditAnywhere, Category = WaterBodies, meta = (UIMin = "0", UIMax = "12", DisplayName = "Build Levels"))
+	int32 BuildLevels = 3;
+
+#if WITH_EDITORONLY_DATA
+	// Approximate size of built water bodies texture
+	// After changing RVT settings, this will update after changing BuildLevels
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = WaterBodies, Transient)
+	int32 TextureSize = 0;
+#endif
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = WaterBodies)
+	TObjectPtr<UTextureRenderTarget2D> WaterBodiesRenderTarget = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Transient, Category = WaterBodies, AdvancedDisplay)
+	TObjectPtr<UMaterialInstanceDynamic> CopyMaterialInstance;
+
+	UFUNCTION()
+	void Init();
+
+	UFUNCTION(BlueprintCallable, Category=WaterBodies)
+	void CaptureRT();
+
+private:
+	UPROPERTY(VisibleAnywhere, Transient, Category = WaterBodies, AdvancedDisplay)
+	TObjectPtr<UTexture2D> BakedWaterBodies;
+};
